4211864d6d8c51d2f631b3247b00dee1bccd107b
Anibal Aguila <aa@usen.io>
2023-05-20 20:40:48 +0200

    adding: staic site

--- infospot/docusaurus.config.js
+++ infospot/docusaurus.config.js
@@ -38,19 +38,21 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
+       // blog: {
+       //   showReadingTime: true,
+       //   // Please change this to your repo.
+       //   // Remove this to remove the "edit this page" links.
+       //   editUrl:
+       //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+       // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -87,45 +89,45 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
+        //  {
+        //    title: 'Docs',
+        //    items: [
+        //      {
+        //        label: 'Tutorial',
+        //        to: '/docs/intro',
+        //      },
+        //    ],
+        //  },
+        //  {
+        //    title: 'Community',
+        //    items: [
+        //      {
+        //        label: 'Stack Overflow',
+        //        href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //      },
+        //      {
+        //        label: 'Discord',
+        //        href: 'https://discordapp.com/invite/docusaurus',
+        //      },
+        //      {
+        //        label: 'Twitter',
+        //        href: 'https://twitter.com/docusaurus',
+        //      },
+        //    ],
+        //  },
+        //  {
+        //    title: 'More',
+        //    items: [
+        //      {
+        //        label: 'Blog',
+        //        to: '/blog',
+        //      },
+        //      {
+        //        label: 'GitHub',
+        //        href: 'https://github.com/facebook/docusaurus',
+        //      },
+        //    ],
+        //  },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
