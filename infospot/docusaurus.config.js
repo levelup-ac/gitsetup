3974dd8536a6f17a75685f554aa0705ac7cceb8a
Anibal Aguila <aa@usen.io>
2023-05-20 20:41:42 +0200

    adding: staic site

--- infospot/docusaurus.config.js
+++ infospot/docusaurus.config.js
@@ -72,18 +72,18 @@ const config = {
           src: 'img/logo.svg',
         },
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+        //  {
+        //    type: 'docSidebar',
+        //    sidebarId: 'tutorialSidebar',
+        //    position: 'left',
+        //    label: 'Tutorial',
+        //  },
+        //  {to: '/blog', label: 'Blog', position: 'left'},
+        //  {
+        //    href: 'https://github.com/facebook/docusaurus',
+        //    label: 'GitHub',
+        //    position: 'right',
+        //  },
         ],
       },
       footer: {
