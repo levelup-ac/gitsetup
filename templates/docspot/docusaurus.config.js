4df34a3173672d42c97c720b795e472fdd871106
Anibal Aguila <aa@usen.io>
2023-05-21 17:57:35 +0200

    ADD: manipulate template feature

--- templates/docspot/docusaurus.config.js
+++ templates/docspot/docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'PROJECT_NAME',
+  tagline: 'PROJECT_NAME are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://GITREPO_URL',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ORGANIZATION_NAME', // Usually your GitHub org/user name.
+  projectName: 'PROJECT_NAME', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
