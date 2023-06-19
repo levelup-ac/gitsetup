ec87a595dc1241e0245faa3a4883732c70fec362
Anibal Aguila <aa@usen.io>
2023-06-20 01:55:50 +0200

    UPDATE

--- templates/docspot/docusaurus.config.js
+++ templates/docspot/docusaurus.config.js
@@ -11,10 +11,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://GITREPO_URL',
+  url: 'https://DOMAIN',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // baseUrl: '/',
+  baseUrl: 'BASE_URL/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
